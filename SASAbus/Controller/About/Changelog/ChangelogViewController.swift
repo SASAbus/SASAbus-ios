a4b2c21c07d74becf4bc4c21a18d0ee022fb2407
Alex Lardschneider <alex.lardschneider@gmail.com>
2017-10-26 12:28:35 +0200

    Added changelog

--- SASAbus/Controller/About/Changelog/ChangelogViewController.swift
+++ SASAbus/Controller/About/Changelog/ChangelogViewController.swift
@@ -1,6 +1,15 @@
 import UIKit
+import SwiftyJSON
 
 class ChangelogViewController: UITableViewController, UIToolbarDelegate {
+    
+    struct Item {
+        
+        var title: String
+        var changes: String
+    }
+    
+    var items = [Item]()
 
     init() {
         super.init(nibName: "ChangelogViewController", bundle: nil)
@@ -12,7 +21,74 @@ class ChangelogViewController: UITableViewController, UIToolbarDelegate {
         super.init(coder: aDecoder)
     }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        parseData()
+    }
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "changelog") ??  UITableViewCell(style: .default, reuseIdentifier: "changelog")
+        
+        let item = items[indexPath.section]
+        
+        cell.textLabel?.text = item.changes
+        cell.textLabel?.numberOfLines = 0
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return items[section].title
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    func parseData() {
+        var language = Utils.locale()
+        
+        if language != "de" || language != "it" || language != "en" {
+            language = "en"
+        }
+        
+        let fileName = "changelog_\(language)"
+        
+        guard let url = Bundle.main.url(forResource: fileName, withExtension: "json") else {
+            Log.error("Could not open json file '\(fileName)'")
+            return
+            
+        }
+        
+        let string = try! String(contentsOf: url, encoding: .utf8)
+        
+        let json = JSON(parseJSON: string)
+        
+        for entry in json.arrayValue {
+            let changes = entry["changes"].arrayValue.map {
+                $0.stringValue
+            }.joined(separator: "\n")
+            
+            let item = Item(
+                title: entry["title"].stringValue,
+                changes: changes
+            )
+            
+            items.append(item)
+        }
+        
+        tableView.reloadData()
     }
 }
