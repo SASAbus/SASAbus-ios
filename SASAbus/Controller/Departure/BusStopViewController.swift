294de190ef603393a2411623bcea6991d445e22a
Alex Lardschneider <alex.lardschneider@gmail.com>
2017-12-13 20:07:59 +0100

    Fixed TabBar items not getting deselected

--- SASAbus/Controller/Departure/BusStopViewController.swift
+++ SASAbus/Controller/Departure/BusStopViewController.swift
@@ -1,25 +1,3 @@
-//
-// BusStopViewController.swift
-// SASAbus
-//
-// Copyright (C) 2011-2015 Raiffeisen Online GmbH (Norman Marmsoler, Jürgen Sprenger, Aaron Falk) <info@raiffeisen.it>
-//
-// This file is part of SASAbus.
-//
-// SASAbus is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// SASAbus is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with SASAbus. If not, see <http://www.gnu.org/licenses/>.
-//
-
 import UIKit
 
 import RxSwift
@@ -151,7 +129,7 @@ class BusStopViewController: MasterViewController, UITabBarDelegate, StatefulVie
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        tabBar.deselect
+        tabBar.selectedItem = nil
     }
     
 
