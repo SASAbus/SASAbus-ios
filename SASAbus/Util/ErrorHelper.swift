45202d4d9c1867858d94ed1a7dec60545c9d0575
Alex Lardschneider <alex.lardschneider@gmail.com>
2017-12-28 15:06:05 +0100

    Skip network errors in Crashlytics

--- SASAbus/Util/ErrorHelper.swift
+++ SASAbus/Util/ErrorHelper.swift
@@ -11,6 +11,22 @@ class ErrorHelper {
         }
 
         #if RELEASE
+            let code = (error as NSError).code
+            
+            let ignore = [
+                NSURLErrorResourceUnavailable,
+                NSURLErrorTimedOut,
+                NSURLErrorNetworkConnectionLost,
+                NSURLErrorCannotFindHost,
+                NSURLErrorInternationalRoamingOff,
+                NSURLErrorServerCertificateUntrusted
+            ]
+            
+            if ignore.contains(code) {
+                // Skip network connection errors
+                return
+            }
+            
             if let message = message {
                 Crashlytics.sharedInstance().recordError(error, withAdditionalUserInfo: ["message": message])
             } else {
